13bba0893f58e14ea80b22f7f8659cbd36b3f9e3
Mikhail Kormyshov <kormyshov@yandex.ru>
2025-01-09 01:47:14 +0300

    Init ci/cd

---
+++ index.py
@@ -0,0 +1,26 @@
+
+def handler(event, context):
+
+    # if event['httpMethod'] == 'GET' and event['headers']['Origin'] == 'https://awesome-index.website.yandexcloud.net':
+    if event['httpMethod'] == 'GET':
+        if event['queryStringParameters']['user'] == '64906703':
+            if event['queryStringParameters']['method'] == 'get_tasks':
+                return {
+                    'statusCode': 200,
+                    'body': '''
+                        {
+                            "projects": [{"id": "1", "projectName": "Написать таск-менеджер", "projectDescription": "Не отвлекайся", "projectStatus": "ACTIVE"}], 
+                            "tasks": [
+                                {"id": "2", "taskName": "Сделать бэк", "taskDescription": "Нормальный, а не то, что сейчас", "isChecked": false, "taskStatus": ""}
+                            ]
+                        }
+                    ''',
+                }
+
+    print(event)
+    print(context)
+
+    return {
+        'statusCode': 200,
+        'body': '{"projects": [], "tasks": []}',
+    }
